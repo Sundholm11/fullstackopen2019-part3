52bcbc4176fd63ecaf567cdd72e3df82924a0fdb
Daniel Sundholm <dajsun@utu.fi>
2019-07-11 13:10:22 +0300

    frontend

---
+++ build/precache-manifest.06afd3b5da68086ba79dc3b8974e7517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563a9f63df4bffb8f83ba08e50881041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9420c0a863b17572bd",
+    "url": "/static/css/main.03adb2dc.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "3a9420c0a863b17572bd",
+    "url": "/static/js/main.a4c9f4e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
